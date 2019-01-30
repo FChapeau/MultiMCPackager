19500a4a4730c1f194b4c0fa9151aeb812958d20
François Drouin-Morin <francois.drouin-morin@frimastudio.com>
2019-01-30 15:29:39 -0500

    Added plenty of documentation

--- MultiMCPackager/__main__.py
+++ MultiMCPackager/__main__.py
@@ -13,10 +13,21 @@ def main(ctx):
 
 @main.command()
 @click.option("--server", default=False,help="Includes client-side mods or not", is_flag=True)
-@click.option("--output", "-o", help="Package output dir", default="./package", type=click.Path(exists=False, file_okay=False, resolve_path=True))
+@click.option("--output", "-o",
+              help="Package output dir",
+              default="./package",
+              type=click.Path(exists=False, file_okay=False, resolve_path=True))
 @click.argument("instancepathstr", metavar="INSTANCE", nargs=1, type=click.Path(exists=True, file_okay=False, resolve_path=True))
 @click.pass_context
 def package(ctx, server, instancepathstr, output):
+    """
+    Packages a MultiMC instance into a package deployable onto a server or to Technic Launcher or other platform
+    :param ctx: Click context
+    :param server: Wether the package is client-side or not
+    :param instancepathstr: Path to the instance, as string
+    :param output: Package output directory
+    """
+
     instancepath = pathlib.Path(instancepathstr)
     outputpath = pathlib.Path(output)
     outputpath.mkdir(exist_ok=True)
@@ -35,6 +46,12 @@ def package(ctx, server, instancepathstr, output):
         instance.fetch_forge(outputpath/"forge.jar")
     else:
         instance.fetch_forge(outputpath/"bin"/"modpack.jar")
+
+    if server:
+        instance.fetch_minecraft(outputpath/"minecraft.jar")
+
+    #TODO Add argument to accept EULA
+    #TODO Generate startup script if server
 
 
 if __name__ == "__main__":
--- MultiMCPackager/instance.py
+++ MultiMCPackager/instance.py
@@ -7,7 +7,15 @@ import shutil
 
 
 class Instance(object):
+    """
+    Represents a MultiMC instance
+    """
+
     def __init__(self, path):
+        """
+        Default constructor, requires the path to the instance
+        :param path: Path to the instance
+        """
         self.instance_path = Path(path)
 
         with open(self.instance_path / "instance.cfg") as f:
@@ -38,19 +46,53 @@ class Instance(object):
         return self.config["name"]
 
     def fetch_forge(self, destination: Path):
+        """
+        Fetch Minecraft Forge jar
+        :param destination: Where to send the forge jar file
+        """
+
         forge_fetch_url = f"https://files.minecraftforge.net/maven/net/minecraftforge/forge/{self.minecraft_version}-{self.forge_version}/forge-{self.minecraft_version}-{self.forge_version}-universal.jar"
         destination.parents[0].mkdir(parents=True, exist_ok=True)
 
         if destination.exists():
             destination.unlink()
 
-        print("Fetching forge")
+        print(f"Fetching forge {self.forge_version}")
         urllib.request.urlretrieve(forge_fetch_url, destination)
 
-    def fetch_minecraft(self, destination: Path):
-        pass
+    def fetch_minecraft(self, destination: Path, server: bool = True):
+        """
+        Fetch Minecraft executable jar from the CDN
+        :param destination: Where to send the downloaded file
+        :param server: Wether to fetch the server or client executable
+        """
+
+        # Fetch version manifest
+        # https://launchermeta.mojang.com/mc/game/version_manifest.json
+        click.echo(f"Fetching Minecraft {self.minecraft_version}")
+        with urllib.request.urlopen("https://launchermeta.mojang.com/mc/game/version_manifest.json") as url:
+            version_manifest = json.loads(url.read().decode())
+            versionlist: list = version_manifest.get("versions")
+            version = next((item for item in versionlist if item["id"] == self.minecraft_version))
+            versionurl = version["url"]
+
+        # Fetch version specific information
+
+        with urllib.request.urlopen(versionurl) as url:
+            manifest = json.loads(url.read().decode())
+            jarurl = manifest["downloads"]["server" if server else "client"]["url"]
+
+        # Fetch server jar based on that
+        urllib.request.urlretrieve(jarurl, destination)
 
     def copy_mods(self, destination: Path, server: bool):
+        """
+        Copies the mod folder to the destination folder
+
+        If the server argument is true, it excludes clientonly mods
+        :param destination: Destination where the mods will be sent
+        :param server: Toggle telling wether the modpack is for a server or not=
+        """
         destination.mkdir(exist_ok=True)
 
         click.echo("Copying mods")
@@ -59,7 +101,12 @@ class Instance(object):
                 if not (server and "clientonly" in child.name):
                     shutil.copyfile(child, destination / child.name)
 
+
     def copy_config(self, destination: Path):
+        """
+        Copies the mod config folder
+        :param destination: Destination where the config will be sent
+        """
         click.echo("Copying config")
         if not (destination).exists():
             shutil.copytree(self.instance_path / ".minecraft" / "config", destination )
